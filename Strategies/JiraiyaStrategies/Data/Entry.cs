3eb2da23c2569a24350c656a5f93167487808309
Jean Alves <jeanc.alves@hotmail.com>
2020-07-16 20:15:48 -0300

    Created the Entry class

---
+++ Strategies/JiraiyaStrategies/Data/Entry.cs
@@ -0,0 +1,16 @@
+﻿namespace NinjaTrader.NinjaScript.Strategies.JiraiyaStrategies
+{
+    public class Entry
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double StopPrice { get; set; }
+        public double TargetPrice { get; set; }
+
+        public enum EntrySide
+        {
+            Long,
+            Short
+        }
+    }
+}
